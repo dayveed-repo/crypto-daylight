98a54f13a5cd4e37e1bdeb13b32b33f3f444a4ec
Ebireni David <ebirenidavid@gmail.com>
2022-08-22 15:03:02 +0100

    fixed app bug

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleSidebar } from "../redux/reducer";
 
